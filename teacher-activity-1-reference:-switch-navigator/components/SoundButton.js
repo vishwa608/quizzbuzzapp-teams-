80d6f41ca934806a4507312548c6313c28ccaaa1
vishwa608 <61463107+vishwa608@users.noreply.github.com>
2020-10-13 18:50:38 +0530

    Add files via upload

---
+++ teacher-activity-1-reference:-switch-navigator/components/SoundButton.js
@@ -0,0 +1,45 @@
+import * as React from 'react';
+import { Text, View, TouchableOpacity, StyleSheet } from 'react-native';
+import {Audio} from 'expo-av';
+
+class SoundButton extends React.Component {
+   playSound = async () => {
+    await Audio.Sound.createAsync(
+      { uri: 'http://soundbible.com/mp3/Buzzer-SoundBible.com-188422102.mp3' },
+      { shouldPlay: true }
+    );
+  }
+
+  render() {
+    return (
+      <TouchableOpacity
+        style={[styles.button,{backgroundColor:this.props.color}]}
+        onPress={this.playSound}>
+        <Text
+          style={styles.buttonText}>
+          Press Me
+        </Text>
+      </TouchableOpacity>
+    );
+  }
+}
+const styles = StyleSheet.create({
+  button: {
+    marginTop: 100,
+    marginLeft: 80,
+    borderWidth: 1,
+    borderColor: 'rgba(0,0,0,0.2)',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 200,
+    height: 200,
+    backgroundColor: 'red',
+    borderRadius: 100,
+  },
+  buttonText: {
+    fontWeight: 'bold',
+    fontSize: 20,
+  }
+});
+
+export default SoundButton;
